b6058f8b03277a46fb28ab1e43409dbb188d8963
Vodangkydev <165755675+Vodangkydev@users.noreply.github.com>
2025-11-28 19:23:48 +0700

    dsa

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,12 @@
-import axios from "axios"
+import axios from "axios";
+
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  (import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://be-cloudy.onrender.com/api");
 
 export const axiosInstance = axios.create({
-     baseURL: "http://localhost:5001/api" || "https://be-cloudy.onrender.com",
-    withCredentials: true,
+  baseURL,
+  withCredentials: true,
 });
