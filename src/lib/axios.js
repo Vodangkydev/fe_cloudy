d2cdafe57c51b035fc367172caefc2df4f798cc4
Vodangkydev <165755675+Vodangkydev@users.noreply.github.com>
2025-11-28 13:37:30 +0700

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+     baseURL: "http://localhost:5001/api" || "https://fe-cloudy.vercel.app",
     withCredentials: true,
 });
